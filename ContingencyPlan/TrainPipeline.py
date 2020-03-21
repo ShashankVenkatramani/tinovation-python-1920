0656016b593179a6e70d0b414d342a99a23e47b9
ShashankVenkatramani <cjvshashank@gmail.com>
2020-03-21 15:15:20 -0800

    Added KMeans code, added new array for UID, seperated UID from old array to prevent it from interfering with dataframe

--- ContingencyPlan/TrainPipeline.py
+++ ContingencyPlan/TrainPipeline.py
@@ -26,15 +26,37 @@ db = firebase.database()
 
 def downloadTrainingData():
     data = []
+    dataUID = []
     users = db.child("users").get().val()
     requests = db.child("requests").get().val()
     for userUID in users:
         user = users[userUID]
-        data.append([userUID, user["photography"], user["pokemon"], user["robotics"], user["scientology"], user["technology"]])
+        data.append([user["photography"], user["pokemon"], user["robotics"], user["scientology"], user["technology"]])
+        dataUID.append(userUID)
     for requestUID in requests:
         request = requests[requestUID]
-        data.append([requestUID, request["photography"], request["pokemon"], request["robotics"], request["scientology"], request["technology"]])
-    print(data)
+        data.append([request["photography"], request["pokemon"], request["robotics"], request["scientology"], request["technology"]])
+        dataUID.append(requestUID)
+    df_cluster_data = pd.DataFrame(data, columns = ["photography", "pokemon", "robotics", "scientology", "technology"])
+    print(df_cluster_data)
+    print(data[197])
+    print(dataUID[197])
+    pca = PCA(n_components = 2, random_state=1)
+    df_cluster_pca = pca.fit_transform(df_cluster_data)
+    print("Explained Variance Ratio: " + str(df_cluster_pca.explained_variance_ratio_.cumsum()[1]))
+
+    distortions = []
+    K_count_range = range(1,10)
+
+    for i in K_to_try:
+        model = KMeans(n_clusters=i, init='k-means++', random_state=1)
+        model.fit(df_cluster_pca)
+        distortions.append(model.inertia_)
+
+    plt.plot(K_to_try, distortions, marker='o')
+    plt.xlabel('Number of Clusters (k)')
+    plt.ylabel('Distortion Amount')
+    plt.show()
 
 def random_generator(size = 32, chars = string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))

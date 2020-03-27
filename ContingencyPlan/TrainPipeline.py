dde15756c36ac719416f4bea34306711835d4bac
ShashankVenkatramani <cjvshashank@gmail.com>
2020-03-26 19:37:46 -0800

    Added clustering, as well as graphing points/Centroids, added PCA optimizer chart

--- ContingencyPlan/TrainPipeline.py
+++ ContingencyPlan/TrainPipeline.py
@@ -38,23 +38,59 @@ def downloadTrainingData():
         data.append([request["photography"], request["pokemon"], request["robotics"], request["scientology"], request["technology"]])
         dataUID.append(requestUID)
     df_cluster_data = pd.DataFrame(data, columns = ["photography", "pokemon", "robotics", "scientology", "technology"])
+    print("Data succesfully placed into Pandas Dataframe")
 
-    pca = PCA(n_components = 2, random_state=1)
-    df_cluster_pca = pca.fit_transform(df_cluster_data)
-    print("Explained Variance Ratio: " + str(pca.explained_variance_ratio_.cumsum()[1]))
+
+
+    pca = PCA().fit(df_cluster_data)
+    cluster_pca = PCA(n_components = 2, random_state=0).fit_transform(df_cluster_data)
+
+    plt.figure()
+    plt.plot(np.cumsum(pca.explained_variance_ratio_))
+    plt.xlabel('Number of Components')
+    plt.xlabel('Variance')
+    plt.show()
+
+    pca = PCA(n_components = 2, random_state=0)
+    cluster_pca = pca.fit_transform(df_cluster_data)
+    print("PCA reduction succesfull")
 
     distortions = []
     K_count_range = range(1,10)
 
     for i in K_count_range:
-        model = KMeans(n_clusters=i, init='k-means++', random_state=1)
-        model.fit(df_cluster_pca)
+        model = KMeans(n_clusters=i, random_state=0)
+        model.fit(cluster_pca)
         distortions.append(model.inertia_)
-
+    print("Plotting for optimal KCount")
     plt.plot(K_count_range, distortions, marker='o')
     plt.xlabel('Number of Clusters (k)')
     plt.ylabel('Distortion Amount')
     plt.show()
+
+    print("Creating final Model")
+    model = KMeans(n_clusters=5, random_state=0)
+    final_model = model.fit(cluster_pca)
+
+    cluster_number = model.predict(cluster_pca)
+
+    print("Plotting clusters")
+    plt.scatter(cluster_pca[cluster_number == 0, 0], cluster_pca[cluster_number == 0, 1], s = 50, c = 'blue', label = 'Cluster 1')
+    plt.scatter(cluster_pca[cluster_number == 1, 0], cluster_pca[cluster_number == 1, 1], s = 50, c = 'green', label = 'Cluster 2')
+    plt.scatter(cluster_pca[cluster_number == 2, 0], cluster_pca[cluster_number == 2, 1], s = 50, c = 'purple', label = 'Cluster 3')
+    plt.scatter(cluster_pca[cluster_number == 3, 0], cluster_pca[cluster_number == 3, 1], s = 50, c = 'yellow', label = 'Cluster 4')
+    plt.scatter(cluster_pca[cluster_number == 4, 0], cluster_pca[cluster_number == 4, 1], s = 50, c = 'red', label = 'Cluster 5')
+
+    plt.scatter(final_model.cluster_centers_[:, 0], final_model.cluster_centers_[:, 1], s=100, c = 'black', label = 'Centroids')
+    plt.title('Clusters of Students')
+    plt.xlabel('PCA 1')
+    plt.ylabel('PCA 2')
+    plt.legend()
+    plt.grid()
+    plt.show()
+
+
+
 
 def random_generator(size = 32, chars = string.ascii_uppercase + string.digits):
     return ''.join(random.choice(chars) for x in range(size))
